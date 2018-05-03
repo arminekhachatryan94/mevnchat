6fe487979d70f5bbc3c5076bbedc6920c9e57579
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-05-02 19:01:00 -0700

    created navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <navbar></navbar>
+    <div class="margin">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: { Navbar }
 }
 </script>
 
 <style>
+.margin {
+  margin-top: 30px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav class="text-white row col-md-12">
+        <span class="col-md-6 text-left">
+            <a id="title" href="/#/">iChat</a>
+        </span>
+        <div class="col-md-6 text-right">
+            <a class="align-middle" href="/#/register">Register</a>
+            <a class="align-middle" href="/#/login">Login</a>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+nav{
+    background-color: rgb(145, 204, 105);
+    width: 100%;
+}
+a{
+    color: white;
+    font-size: 20px;
+    padding: 5px;
+}
+#title {
+    font-size: 30px;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
+}
+#height {
+    height: 100px;
+}
+</style>
