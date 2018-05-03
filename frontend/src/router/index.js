341f5c08543155a3207174951e60008b0a79ddb4
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-05-02 19:06:19 -0700

    changed frontend structure

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Login from '@/components/Login'
+import Login from '@/components/views/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/'
     },
     {
       path: '/login',
